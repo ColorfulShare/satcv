a382842c9f583aaacb42c00e51dc00adb82ea33f
Leomiguel19 <leomiguel1907@gmail.com>
2021-08-24 18:03:10 -0400

    Agregada Migracion de usuarios y de direccion

---
+++ database/migrations/2013_08_24_195359_create_locations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->string('address')->nullable();
+            $table->string('district')->nullable();
+            $table->string('city')->nullable();
+            $table->string('department')->nullable();
+            $table->longtext('photoDB')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,20 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
+            $table->longtext('dni')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('birth')->nullable(); 
+            $table->date('dni_expedition')->nullable(); 
+            $table->string('phone')->nullable();      
+            $table->string('mobile_phone')->nullable();      
+            $table->string('city_dni')->nullable();      
+            $table->timestamp('email_verified_at')->nullable();
+            $table->enum('status', [0, 1, 2])->default(0)->comment('0 - inactivo, 1 - activo, 2 - eliminado');        
+            $table->longtext('photoDB')->nullable();
+            $table->longtext('photo_dni')->nullable();
+            $table->foreignId('location_id')->nullable()->constrained('locations');
             $table->rememberToken();
             $table->timestamps();
         });
