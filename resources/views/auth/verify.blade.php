b1cca8dbe225e611ec520a6c168d2b607eb2a444
William Ache <william28ache@gmail.com>
2021-08-25 12:59:48 -0400

    fix send email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts/fullLayoutMaster')
 
-@section('title', 'Login Page')
+@section('title', 'verify email')
 
 @section('page-style')
 {{-- Page Css files --}}
-<link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
-<link rel="stylesheet" href="{{ asset(mix('css/base/pages/page-auth.css')) }}">
+<link rel="stylesheet" href="{{ asset('css/base/plugins/forms/form-validation.css') }}">
+<link rel="stylesheet" href="{{ asset('css/base/pages/page-auth.css') }}">
 @endsection
 
-@section('content')
 <div class="auth-wrapper auth-v1 px-2">
   <div class="auth-inner py-2">
     <!-- Login v1 -->
@@ -51,7 +50,7 @@
         <p class="card-text mb-2">{{ __('Before proceeding, please check your email for a verification link.') }}</p>
         <p class="card-text">{{ __('If you did not receive the email') }},
 
-          <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+          <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
             @csrf
             <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
           </form>
@@ -60,4 +59,3 @@
     </div>
   </div>
 </div>
-@endsection
