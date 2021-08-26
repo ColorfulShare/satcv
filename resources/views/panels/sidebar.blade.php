53263129fad4a45a97e198e8a32b23e60adfc0a6
eulinp <eulinpr@gmail.com>
2021-08-26 07:42:16 -0400

    Menus y dashboard

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -45,9 +45,49 @@ $configData = Helper::applClasses();
   <div class="shadow-bottom"></div>
   <div class="main-menu-content">
     <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+      
+      {{-- Para Usuarios Normales --}}
+      @if (Auth::user()->admin == 0)
       {{-- Foreach menu item starts --}}
-      @if(isset($menuData[0]))
-      @foreach($menuData[0]->menu as $menu)
+        @if(isset($menuData[0]))
+        @foreach($menuData[0]->menu as $menu)
+        @if(isset($menu->navheader))
+        <li class="navigation-header">
+          <span>{{ __('locale.'.$menu->navheader) }}</span>
+          <i data-feather="more-horizontal"></i>
+        </li>
+        @else
+        {{-- Add Custom Class with nav-item --}}
+        @php
+        $custom_classes = "";
+        if(isset($menu->classlist)) {
+        $custom_classes = $menu->classlist;
+        }
+        @endphp
+        <li class="nav-item {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }} {{ $custom_classes }}">
+          <a href="{{isset($menu->url)? url($menu->url):'javascript:void(0)'}}" class="d-flex align-items-center" target="{{isset($menu->newTab) ? '_blank':'_self'}}">
+            <i data-feather="{{ $menu->icon }}"></i>
+            <span class="menu-title text-truncate">{{ __('locale.'.$menu->name) }}</span>
+            @if (isset($menu->badge))
+            <?php $badgeClasses = "badge badge-pill badge-light-primary ml-auto mr-1" ?>
+            <span class="{{ isset($menu->badgeClass) ? $menu->badgeClass : $badgeClasses }} ">{{$menu->badge}}</span>
+            @endif
+          </a>
+          @if(isset($menu->submenu))
+          @include('panels/submenu', ['menu' => $menu->submenu])
+          @endif
+        </li>
+        @endif
+        @endforeach
+        @endif
+        {{-- Foreach menu item ends --}}
+
+        {{-- para usuarios admin --}}
+      @else
+      
+      {{-- Foreach menu item starts --}}
+      @if(isset($menuData[1]))
+      @foreach($menuData[1]->menu as $menu)
       @if(isset($menu->navheader))
       <li class="navigation-header">
         <span>{{ __('locale.'.$menu->navheader) }}</span>
@@ -78,6 +118,7 @@ $configData = Helper::applClasses();
       @endforeach
       @endif
       {{-- Foreach menu item ends --}}
+    @endif
     </ul>
   </div>
 </div>
