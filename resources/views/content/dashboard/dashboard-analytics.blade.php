5435d1c5931c365c10e9c5e6129f6c3a3cb785f9
eulinp <eulinpr@gmail.com>
2021-08-26 12:57:15 -0400

    Modificacion contratos dashboard

--- resources/views/content/dashboard/dashboard-analytics.blade.php
+++ resources/views/content/dashboard/dashboard-analytics.blade.php
@@ -22,22 +22,30 @@
             <div class="card-body">
                 <div class="profile-image-wrapper">
                     <div class="profile-image text-center">
-                        <div class="avatar">
-                            <img src="http://127.0.0.1:8000/images/portrait/small/avatar-s-9.jpg" alt="Profile Picture">
+                        <div class="avatar my-2">
+                            @if (Auth::user()->profile_photo_path != NULL)
+                                <img width="110px" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}">
+                            @else
+                                <img width="110px" src="https://ui-avatars.com/api/?background=random&name={{ Auth::user()->name }}" alt="{{ Auth::user()->name }}">                                
+                            @endif
                         </div>
                     </div>
                 </div>
-                <h3 class="text-center">Curtis Stone</h3>
-                <h6 class="text-muted font-weight-bolder text-center">Bitcoin Ecuador ID <span>3683</span></h6>
+                <h3 class="text-center">{{Auth::user()->name}}</h3>
+                <h6 class="text-muted font-weight-bolder text-center">Bitcoin Ecuador ID #<span>{{ Auth::user()->id }}</span></h6>
+                @if(Auth::user()->email_verified_at != null)
                 <h5 class="text-success font-weight-bolder text-center">Usuario Verificado</h5>
-
+                @else
+                <h5 class="text-danger font-weight-bolder text-center">Usuario No Verificado</h5>
+                @endif
+                @if(count($contratos)>0)
 				<div class="row justify-content-center my-2">
 					<div class="form-group col-8">
 						<label for="selectContract" class="d-flex justify-content-center">Seleccione un contrato para su información</label>
                           <select class="form-control fa" id="selectContract">
                               <option>-- Seleccione un contrato --</option>
-							@foreach($ordenes as $orden)
-                            <option value="{{$orden->id}}">Contrato #: {{$orden->id}} / {{date_format($orden->created_at,"Y/m/d")}}</option>
+							@foreach($contratos as $contrato)
+                            <option value="{{$contrato->id}}">Contrato #: {{$contrato->id}} / {{date_format($contrato->created_at,"Y/m/d")}}</option>
                             @endforeach
 						</select>
 					</div>
@@ -50,112 +58,76 @@
                     <div class="text-center">
                         <i class="fa fa-briefcase fa-3x"></i>
                         <h6 class="text-muted font-weight-bolder">Inversión</h6>
-                        <h3 class="mb-0" id="contratoInversion"></h3>
+                        <h3 class="mb-0" id="contratoInversion">0</h3>
                     </div>
                     <div class="text-center">
                         <i class="fa fa-wallet fa-3x"></i>
                         <h6 class="text-muted font-weight-bolder">Saldo Capital</h6>
-                        <h3 class="mb-0" id="contratoSaldoCapital"></h3>
+                        <h3 class="mb-0" id="contratoSaldoCapital">0</h3>
                     </div>
                     <div class="text-center">
                         <i class="fa fa-chart-line fa-3x"></i>
                         <h6 class="text-muted font-weight-bolder">Productividad</h6>
-                        <h3 class="mb-0" id="contratoProductividad"></h3>
+                        <h3 class="mb-0" id="contratoProductividad">0</h3>
                     </div>
                     <div class="text-center">
                         <i class="fa fa-money-bill fa-3x"></i>
                         <h6 class="text-muted font-weight-bolder">Retirado</h6>
-                        <h3 class="mb-0" id="contratoRetirado"></h3>
+                        <h3 class="mb-0" id="contratoRetirado">0</h3>
                     </div>
                 </div>
+                @else
+                <div class="row justify-content-center my-2">
+					<div class="form-group col-8">
+                        <h6 class="mb-0 h2 text-center">Aún no hay contratos</h6>
+					</div>
+				</div>
+                @endif
             </div>
         </div>
 
-        <div id="logs-list">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header p-1">
-                        <div class="head-label">
-                            <h6 class="mb-0 h2">Historial de Rendimiento</h6>
-                        </div>
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header p-1">
+                    <div class="head-label">
+                        <h6 class="mb-0 h2">Historial de Rendimiento</h6>
                     </div>
-                    <div class="card-content">
-                        <div class="card-body card-dashboard">
-                            <div class="table-responsive">
-                                <table class="table w-100 nowrap scroll-horizontal-vertical myTable table-striped w-100">
-                                    <thead class="">
-        
-                                        <tr class="text-center text-white bg-purple-alt2">                                
-                                            <th>ID</th>
-                                            <th>Correo</th>
-                                            <th>Transaccion</th>
-                                            <th>Tipo de interes</th>
-                                            <th>Monto</th>
-                                            <th>Estado</th>
-                                            <th>Fecha de Creación</th>
-                                        </tr>
-        
-                                    </thead>
-                                    <tbody>
-        
-                                        @foreach ($ordenes as $orden)
-                                        <tr class="text-center">
-                                            <td>{{$orden->id}}</td>
-                                            <td>{{$orden->user->email}}</td>
-                                            <td>{{$orden->transaction_id}}</td>
-                                            <td>{{$orden->type_interes}}</td>
-                                            <td>{{$orden->amount}}</td>
-                                            <td>
-                                                <button type="button"
-                                                @if (Auth::user()->admin == 1 && $orden->status == '0')
-                                                data-toggle="modal"
-                                                data-target="#ModalStatus{{$orden->id}}"
-                                                @endif
-                                                class="@if ($orden->status == '0') btn btn-info text-white text-bold-600  @elseif($orden->status == '1') btn btn-success text-white text-bold-600 @elseif($orden->status >= '2') btn btn-danger text-white text-bold-600 @endif">{{$orden->status()}}
-                                                </button>
-                                            </td>
-                                            <td>{{$orden->created_at->format('Y-m-d')}}</td>
-        
-                                        </tr>
-                                        @if (Auth::user()->admin == 1 && $orden->status == '0')
-                                            <!-- Modal -->
-                                            <div class="modal fade" id="ModalStatus{{$orden->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                <div class="modal-dialog">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                        <h5 class="modal-title" id="exampleModalLabel">Cambiar estatus</h5>
-                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                        <span aria-hidden="true">&times;</span>
-                                                        </button>
-                                                    </div>
-                                                    <form action="{{ route('cambiarStatus') }}" method="POST">
-                                                    @csrf
-                                                    <div class="modal-body">
-        
-                                                    <input type="hidden" name="id" value="{{$orden->id}}">
-                                                    ¿Desea cambiar es estatus de la orden?
-                                                    <br>
-                                                    <label>Seleccione el estado</label>
-                                                    <select name="status" required class="form-control">
-                                                        <option value="">Seleccione un estado</option>
-                                                        <option value="1">Aprobado</option>
-                                                        <option value="2">Rechazado</option>
-                                                    </select>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                                                        <button type="submit" class="btn btn-primary">Guardar</button>
-                                                    </div>
-                                                    </form>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        @endif
-                                        @endforeach
-                                       
-                                    </tbody>
-                                </table>
-                            </div>
+                </div>
+                <div class="card-content">
+                    <div class="card-body card-dashboard">
+                        <div class="table-responsive">
+                            <table class="table w-100 nowrap scroll-horizontal-vertical myTable table-striped w-100">
+                                <thead class="">
+    
+                                    <tr class="text-center bg-purple-alt2">                                
+                                        <th>Fecha</th>
+                                        <th>Descripción</th>
+                                        <th>Monto</th>
+                                        <th>Porcentaje</th>
+                                    </tr>
+    
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>2021-08-26</td>
+                                        <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, neque?</td>
+                                        <td>200</td>
+                                        <td>10%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2021-08-26</td>
+                                        <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, neque?</td>
+                                        <td>200</td>
+                                        <td>10%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2021-08-26</td>
+                                        <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, neque?</td>
+                                        <td>200</td>
+                                        <td>10%</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -175,31 +147,36 @@
         let contratoProductividad = document.querySelector("#contratoProductividad");
         let contratoRetirado = document.querySelector("#contratoRetirado");
 
-        let url = '/getContrato/';
+        let url = 'api/getContrato/';
         let token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
         selectContract.addEventListener('change', function(){
-            fetch("api"+url+selectContract.value, {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json, text-plain, */*",
-                    "X-Requested-With": "XMLHttpRequest",
-                    "X-CSRF-TOKEN": token
-                    },
-                method: 'get',
-            })
-            .then( response => response.text() )
-            .then( resultText => (
-                data = JSON.parse(resultText),
-                contratoInversion.innerHTML = data.invested,
-                contratoSaldoCapital.innerHTML = data.capital,
-                contratoProductividad.innerHTML = data.gain,
-                contratoRetirado.innerHTML = data.gain
-
-                // console.log(data)
-            ))
-            .catch(function(error) {
-                console.log(error);
-            });
+            if(selectContract.value > 0){
+                fetch(url+selectContract.value, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json, text-plain, */*",
+                        "X-Requested-With": "XMLHttpRequest",
+                        "X-CSRF-TOKEN": token
+                        },
+                    method: 'get',
+                })
+                .then( response => response.text() )
+                .then( resultText => (
+                    data = JSON.parse(resultText),
+                    contratoInversion.innerHTML = data.invested,
+                    contratoSaldoCapital.innerHTML = data.capital,
+                    contratoProductividad.innerHTML = data.gain,
+                    contratoRetirado.innerHTML = data.gain
+                ))
+                .catch(function(error) {
+                    console.log(error);
+                });
+            }else{
+                contratoInversion.innerHTML = 0;
+                contratoSaldoCapital.innerHTML = 0;
+                contratoProductividad.innerHTML = 0;
+                contratoRetirado.innerHTML = 0;
+            }
         });
 
     });
