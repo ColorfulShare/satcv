96005ab06e28db60e13712b5bbc9f6972e7a7a71
eulinp <eulinpr@gmail.com>
2021-08-31 08:16:19 -0400

    Fix NavBar

--- resources/views/panels/scripts.blade.php
+++ resources/views/panels/scripts.blade.php
@@ -1,8 +1,8 @@
 {{-- Vendor Scripts --}}
 <script src="{{ asset('vendors/js/vendors.min.js') }}"></script>
 <script src="{{ asset('vendors/js/ui/prism.min.js') }}"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.6.0/jq-3.3.1/dt-1.11.0/datatables.min.js"></script>
-
+<script src="{{ asset('vendors/js/tables/datatable/datatables.min.js') }}"></script>
+<script src="{{ asset('vendors/js/tables/datatable/dataTables.bootstrap4.min.js') }}"></script>
 <script src="{{ asset('vendors/js/extensions/toastr.min.js') }}"></script>
 <script src="{{ asset('vendors/js/extensions/sweetalert2.all.min.js') }}"></script>
 @yield('vendor-script')
