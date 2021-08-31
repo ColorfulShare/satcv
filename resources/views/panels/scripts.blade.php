2e52f49ac0f894de51f3f1d22f17ea27afa13613
eulinp <eulinpr@gmail.com>
2021-08-31 15:13:08 -0400

    Scripts

--- resources/views/panels/scripts.blade.php
+++ resources/views/panels/scripts.blade.php
@@ -1,18 +1,18 @@
 {{-- Vendor Scripts --}}
-<script src="{{ asset('vendors/js/vendors.min.js') }}"></script>
-<script src="{{ asset('vendors/js/ui/prism.min.js') }}"></script>
-<script src="{{ asset('vendors/js/tables/datatable/datatables.min.js') }}"></script>
-<script src="{{ asset('vendors/js/tables/datatable/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('vendors/js/extensions/toastr.min.js') }}"></script>
-<script src="{{ asset('vendors/js/extensions/sweetalert2.all.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/vendors.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/ui/prism.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/tables/datatable/datatables.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/tables/datatable/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/extensions/toastr.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/extensions/sweetalert2.all.min.js') }}"></script>
 @yield('vendor-script')
 {{-- Theme Scripts --}}
-<script src="{{ asset('js/core/app-menu.js') }}"></script>
-<script src="{{ asset('js/core/app.js') }}"></script>
-@if($configData['blankPage'] === false)
-<script src="{{ asset('js/scripts/customizer.js') }}"></script>
-@endif
+    <script src="{{ asset('js/core/app-menu.js') }}"></script>
+    <script src="{{ asset('js/core/app.js') }}"></script>
+    @if($configData['blankPage'] === false)
+    <script src="{{ asset('js/scripts/customizer.js') }}"></script>
+    @endif
 {{-- page script --}}
-@yield('page-script')
-@stack('custom_js')
+    @yield('page-script')
+    @stack('custom_js')
 {{-- page script --}}
