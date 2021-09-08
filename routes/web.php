1de44299a756b4ea6011ca6ff6cbfc75cfe3bb09
luis briceño <86269979+lkiraval@users.noreply.github.com>
2021-09-08 19:29:45 -0400

    agregado midleware auth a todas las rutas

--- routes/web.php
+++ routes/web.php
@@ -33,121 +33,121 @@ use App\Http\Controllers\Auth\TwoFactorController;
 // Auth::routes(['verify' => true]);
 
 // Main Page Route
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware(['auth']);
+Route::middleware('auth')->group(function(){
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware(['auth']);
 
 
-Route::get('/{id}', [DashboardController::class,'dashboard2'])->name('dashboard2')->middleware(['auth']);
+    Route::get('/{id}', [DashboardController::class,'dashboard2'])->name('dashboard2')->middleware(['auth']);
 
-Route::group(['prefix' => 'contratos'], function () {
-    Route::get('/', [ContractsController::class, 'index'])->name('contract.index');
-    Route::get('/user', [ContractsController::class, 'contratosUser'])->name('contract.user');
-    Route::get('/inversion-data', [ContractsController::class, 'dataInversion'])->name('data.inversion');
-    Route::post('/form-pdf', [ContractsController::class, 'formPdf'])->name('contract.pdf');
-    Route::get('/utilidades', [ContractsController::class, 'utilidades'])->name('contract.utilidades');
-    Route::get('/testCoin', [ContractsController::class, 'testCoin'])->name('contract.testCoin');
-    Route::post('/payUtility', [ContractsController::class, 'payUtility'])->name('payUtility');
-    Route::get('/generatePdf/{id}', [ContractsController::class, 'generatePdf'])->name('contract.generatePdf');
-});
+    Route::group(['prefix' => 'contratos'], function () {
+        Route::get('/', [ContractsController::class, 'index'])->name('contract.index');
+        Route::get('/user', [ContractsController::class, 'contratosUser'])->name('contract.user');
+        Route::get('/inversion-data', [ContractsController::class, 'dataInversion'])->name('data.inversion');
+        Route::post('/form-pdf', [ContractsController::class, 'formPdf'])->name('contract.pdf');
+        Route::get('/utilidades', [ContractsController::class, 'utilidades'])->name('contract.utilidades');
+        Route::get('/testCoin', [ContractsController::class, 'testCoin'])->name('contract.testCoin');
+        Route::post('/payUtility', [ContractsController::class, 'payUtility'])->name('payUtility');
+        Route::get('/generatePdf/{id}', [ContractsController::class, 'generatePdf'])->name('contract.generatePdf');
+    });
 
-Route::group(['prefix' => 'shop'], function () {
-    Route::get('/', [TiendaController::class, 'index'])->name('shop');
-    Route::post('/procces', [TiendaController::class, 'procesarOrden'])->name('shop.procces');
-    Route::post('/ipn', [TiendaController::class, 'ipn'])->name('shop.ipn');
-    Route::post('/{status}/estado', [TiendaController::class, 'statusProcess'])->name('shop.proceso.status');
-    Route::post('/cambiarStatus', [TiendaController::class, 'cambiar_status'])->name('cambiarStatus');
-});
+    Route::group(['prefix' => 'shop'], function () {
+        Route::get('/', [TiendaController::class, 'index'])->name('shop');
+        Route::post('/procces', [TiendaController::class, 'procesarOrden'])->name('shop.procces');
+        Route::post('/ipn', [TiendaController::class, 'ipn'])->name('shop.ipn');
+        Route::post('/{status}/estado', [TiendaController::class, 'statusProcess'])->name('shop.proceso.status');
+        Route::post('/cambiarStatus', [TiendaController::class, 'cambiar_status'])->name('cambiarStatus');
+    });
 
-Route::group(['prefix' => 'reports'], function () {
-    Route::get('/', [ReportController::class, 'index'])->name('reports.pedidos');
-    Route::get('/purchase', [ReportController::class, 'indexPedidos'])->name('reports.index');
-    Route::get('/show-contrato{id}', [ReportController::class, 'indexShow'])->name('reports.show-contrato');
- 
-});
-
-Route::group(['prefix' => 'solicitud'], function () {
-    Route::get('/retiro', [SolicitudController::class, 'index_retiros'])->name('solicitud.retiros')->middleware('primerosCincoDias');
-    Route::get('/retiros', [SolicitudController::class, 'index_solicitudes'])->name('solicitud.remove');
-});
-
-//Ruta de los Tickets
-Route::prefix('ticket')->middleware(['auth'])->group(function(){
-
-    // Para el usuario
-    Route::get('create', [TicketController::class,'create'])->name('ticket.create');
-    Route::post('store', [TicketController::class,'store'])->name('ticket.store'); 
-    Route::get('edit-user/{id}', [TicketController::class,'editUser'])->name('ticket.edit-user');
-    Route::patch('update-user/{id}', [TicketController::class,'updateUser'])->name('ticket.update-user');
-    Route::get('list-user', [TicketController::class,'indexUser'])->name('ticket.list-user');
-    Route::get('show-user/{id}', [TicketController::class,'showUser'])->name('ticket.show-user');
-
-    // Para el Admin
-    Route::get('edit-admin/{id}', [TicketController::class,'editAdmin'])->name('ticket.edit-admin');
-    Route::patch('update-admin/{id}', [TicketController::class,'updateAdmin'])->name('ticket.update-admin');
-    Route::get('list-admin', [TicketController::class,'indexAdmin'])->name('ticket.list-admin');
-});
-
-Route::group(['prefix' => 'utilidad'], function () {
-    Route::get('/', [WalletController::class,'utility'])->name('utilidad.utility');
-});
-
-Route::group(['prefix' => 'retiros'], function () {
-    Route::get('/retirar', [RetirosController::class, 'retirar'])->name('retiros.retirar');
-    Route::post('/retirar', [RetirosController::class, 'retiro'])->name('retiro');
-});
-
-// 2fact
-Route::get('/2fact', [DoubleAutenticationController::class, 'index'])->name('2fact');
-Route::post('/2fact', [DoubleAutenticationController::class, 'checkCodeLogin'])->name('2fact.post');
-
-// locale Route
-/*
-Route::get('lang/{locale}', [LanguageController::class, 'swap']);
-*/
-//rutas para la lista de usuarios
- Route::prefix('user')->group(function(){
-
-    Route::get('/list-user',[UserController::class,'listUser'])->name('users.list-user');
-
-    Route::get('/list-kyc',[UserController::class,'listKyc'])->name('users.list-kyc');
-
-    Route::get('show-user/{id}',[UserController::class,'showUser'])->name('users.show-user');
-
-    Route::get('verificar/{id}',[UserController::class,'verifyUser'])->name('verify-user');
-    Route::get('rechazar/{id}',[UserController::class,'denyUser'])->name('deny-user');
+    Route::group(['prefix' => 'reports'], function () {
+        Route::get('/', [ReportController::class, 'index'])->name('reports.pedidos');
+        Route::get('/purchase', [ReportController::class, 'indexPedidos'])->name('reports.index');
+        Route::get('/show-contrato{id}', [ReportController::class, 'indexShow'])->name('reports.show-contrato');
     
-    Route::get('two_factor_challenge',[UserController::class,'two_factor_challenge'])->name('user.two_factor_challenge');
+    });
+
+    Route::group(['prefix' => 'solicitud'], function () {
+        Route::get('/retiro', [SolicitudController::class, 'index_retiros'])->name('solicitud.retiros')->middleware('primerosCincoDias');
+        Route::get('/retiros', [SolicitudController::class, 'index_solicitudes'])->name('solicitud.remove');
+    });
+
+    //Ruta de los Tickets
+    Route::prefix('ticket')->group(function(){
+
+        // Para el usuario
+        Route::get('create', [TicketController::class,'create'])->name('ticket.create');
+        Route::post('store', [TicketController::class,'store'])->name('ticket.store'); 
+        Route::get('edit-user/{id}', [TicketController::class,'editUser'])->name('ticket.edit-user');
+        Route::patch('update-user/{id}', [TicketController::class,'updateUser'])->name('ticket.update-user');
+        Route::get('list-user', [TicketController::class,'indexUser'])->name('ticket.list-user');
+        Route::get('show-user/{id}', [TicketController::class,'showUser'])->name('ticket.show-user');
+
+        // Para el Admin
+        Route::get('edit-admin/{id}', [TicketController::class,'editAdmin'])->name('ticket.edit-admin');
+        Route::patch('update-admin/{id}', [TicketController::class,'updateAdmin'])->name('ticket.update-admin');
+        Route::get('list-admin', [TicketController::class,'indexAdmin'])->name('ticket.list-admin');
+    });
+
+    Route::group(['prefix' => 'utilidad'], function () {
+        Route::get('/', [WalletController::class,'utility'])->name('utilidad.utility');
+    });
+
+    Route::group(['prefix' => 'retiros'], function () {
+        Route::get('/retirar', [RetirosController::class, 'retirar'])->name('retiros.retirar');
+        Route::post('/retirar', [RetirosController::class, 'retiro'])->name('retiro');
+    });
+
+    // 2fact
+    Route::get('/2fact', [DoubleAutenticationController::class, 'index'])->name('2fact');
+    Route::post('/2fact', [DoubleAutenticationController::class, 'checkCodeLogin'])->name('2fact.post');
+
+    // locale Route
     /*
-    Route::get('/impersonate/stop', 'ImpersonateController@stop')->name('impersonate.stop');
-    Route::post('/impersonate/{user}/start', 'ImpersonateController@start')->name('impersonate.start');
+    Route::get('lang/{locale}', [LanguageController::class, 'swap']);
     */
+    //rutas para la lista de usuarios
+    Route::prefix('user')->group(function(){
+
+        Route::get('/list-user',[UserController::class,'listUser'])->name('users.list-user');
+
+        Route::get('/list-kyc',[UserController::class,'listKyc'])->name('users.list-kyc');
+
+        Route::get('show-user/{id}',[UserController::class,'showUser'])->name('users.show-user');
+
+        Route::get('verificar/{id}',[UserController::class,'verifyUser'])->name('verify-user');
+        Route::get('rechazar/{id}',[UserController::class,'denyUser'])->name('deny-user');
+        
+        Route::get('two_factor_challenge',[UserController::class,'two_factor_challenge'])->name('user.two_factor_challenge');
+        /*
+        Route::get('/impersonate/stop', 'ImpersonateController@stop')->name('impersonate.stop');
+        Route::post('/impersonate/{user}/start', 'ImpersonateController@start')->name('impersonate.start');
+        */
+    });
+
+    //Rutas para las liquidaciones realizadas
+    Route::prefix('liquidations')->group(function(){
+        //Vista de liquidaciones de Capital
+        Route::get('/capital',[LiquidationController::class,'index'])->name('liquidations.index');
+    //Vista de liquidaciones de Comision
+        Route::get('/commissions',[LiquidationController::class,'commissions'])->name('liquidations.history');
+
+
+    });
+    //ruta para administradores de carteras
+    Route::prefix('administrators')->group(function (){
+
+    Route::get('/', [WalletController::class,'administrators'])->name('administrators.index');
+    });
+
+        // Ruta para la pagos
+    Route::prefix('payments')->group(function (){
+    //Vista de pagos Realizados  
+    Route::get('/', [WalletController::class,'payments'])->name('payments.index');
+    });
+
+    Route::get('verify/resend', [TwoFactorController::class, 'resend'])->name('verify.resend');
+    Route::resource('verify', TwoFactorController::class)->only('index', 'store');
 });
-
-//Rutas para las liquidaciones realizadas
-Route::prefix('liquidations')->group(function(){
-    //Vista de liquidaciones de Capital
-    Route::get('/capital',[LiquidationController::class,'index'])->name('liquidations.index');
-//Vista de liquidaciones de Comision
-    Route::get('/commissions',[LiquidationController::class,'commissions'])->name('liquidations.history');
-
-
-});
-//ruta para administradores de carteras
-Route::prefix('administrators')->group(function (){
-
-  Route::get('/', [WalletController::class,'administrators'])->name('administrators.index');
-});
-
-    // Ruta para la pagos
-Route::prefix('payments')->group(function (){
-  //Vista de pagos Realizados  
-  Route::get('/', [WalletController::class,'payments'])->name('payments.index');
- });
-
- Route::get('verify/resend', [TwoFactorController::class, 'resend'])->name('verify.resend');
- Route::resource('verify', TwoFactorController::class)->only('index', 'store');
-
-
-
 
 
 
